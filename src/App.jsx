5354db8c6c45bd6b66e9bc58b19c447c066269d9
Max Welin <maxwwelin@gmail.com>
2025-04-29 22:47:31 +0200

    first commit skills feature branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Hero from "./components/Portfolio/Hero";
+import Skills from "./components/Portfolio/Skills";
 
 function App() {
   return (
     <>
       <h1 className="text-5xl text-center bg-primary">Hello world</h1>
       <p className="text-2xl text-center bg-secondary">from team one</p>
+      <Hero />
+      <Skills />
     </>
   );
 }
---
+++ src/components/Portfolio/Skills.jsx
@@ -0,0 +1,9 @@
+const Skills = () => {
+  return (
+    <div className="bg-black text-xl text-center text-white px-10 py-10">
+      <h1>Max</h1>
+    </div>
+  );
+};
+
+export default Skills;
