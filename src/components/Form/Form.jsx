8133f427d90d22382ae3562dcfbc590894fa4b38
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-04-30 22:00:00 +0200

    Fixade lite med formuläret så det syns bättre

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,58 +1,36 @@
-import React, { useState } from "react";
-
 const Form = () => {
-  // const [inputValue, setInputValue] = useState({
-  //   namn: "",
-  //   efternamn: "",
-  //   skola: "",
-  //   utbildning: "",
-  //   email: "",
-  //   telefon: "",
-  //   github: "",
-  //   linkedIn: "",
-  //   about: "",
-  //   rubrik: "",
-  //   projekt: "",
-  //   skills: "",
-  //   färgTema: "",
-  //   fontTema: "",
-  //   cv: null,
-  //   profilbild: null,
-  // });
-
-  // //hantera flera input value fields
-  // const handleChange = (e) => {
-  //   const { name, value, type, files } = e.target;
-  //   setInputValue((prev) => {
-  //     return { ...prev, [name]: type === "file" ? files[0] : value };
-  //   });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputValue);
   };
 
   return (
-    <div className="">
+    <div className="flex justify-center items-center">
       <form onSubmit={handleSubmit}>
-        <input type="text" name="namn" />
-        <input type="text" name="efternamn" />
-        <input type="text" name="skola" />
-        <input type="text" name="utbildning" />
-        <input type="email" name="email" />
-        <input type="tel" name="telefon" />
-        <input type="url" name="github" />
-        <input type="url" name="linkedIn" />
-        <textarea name="about" />
-        <input type="text" name="rubrik" />
-        <input type="url" name="projekt" />
-        <textarea name="skills" />
-        <input type="color" name="färgTema" />
-        <select name="fontTema" />
-        <input type="file" accept=".pdf" name="cv" />
-        <input type="file" accept="image/*" name="profilbild" />
-        <button type="submit">Submit</button>
+        <div className="flex flex-col gap-2 max-w-[400px] border-2 border-black">
+          <input type="text" name="namn" placeholder="Name..." />
+          <input type="text" name="efternamn" placeholder="Last name..." />
+          <input type="text" name="skola" placeholder="School..." />
+          <input type="text" name="utbildning" placeholder="Education..." />
+          <input type="email" name="email" placeholder="Email..." />
+          <input type="tel" name="telefon" placeholder="Tel..." />
+          <input type="url" name="github" placeholder="Github..." />
+          <input type="url" name="linkedIn" placeholder="LinkedIn..." />
+          <input type="text" name="rubrik" placeholder="Add your header..." />
+          <textarea name="about" placeholder="Om mig"></textarea>
+          <input type="url" name="projekt" placeholder="Olika projekt" />
+          <textarea name="skills" placeholder="Tekniska färdigheter"></textarea>
+          <input type="color" name="färgTema" />
+          <select name="fontTema">
+            <option value="sans">Sans-serif</option>
+            <option value="poppins">Poppins</option>
+            <option value="mono">Monospace</option>
+          </select>
+          <input type="file" accept=".pdf" name="cv" />
+          <input type="file" accept="image/*" name="profilbild" />
+          <button type="submit" className="cursor-pointer">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
