5caa10d75047257b8a05c9714efa7b117c4dbbcb
Max Welin <maxwwelin@gmail.com>
2025-05-05 15:43:55 +0200

    Started working on context/context provider. Not every variable added yet. handleSubmit func in form.jsx working. Not every variable added here either

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Footer from "./components/Portfolio/Footer";
 import Hero from "./components/Portfolio/Hero";
 import Projects from "./components/Portfolio/Projects";
 import Skills from "./components/Portfolio/Skills";
+import { FormContextProvider } from "./components/providers/FormContext";
 
 function App() {
   return (
-    <>
+    <FormContextProvider>
       <div className="grid grid-cols-9 bg-black">
         <div className="col-start-2 col-end-9 pt-[200px]">
           <Hero />
@@ -19,7 +20,7 @@ function App() {
           <Footer />
         </div>
       </div>
-    </>
+    </FormContextProvider>
   );
 }
 
